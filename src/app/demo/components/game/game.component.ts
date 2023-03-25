6d94693f1fc2fa5469c017cd3b6b8650af78bd71
Christian Albán <chris_alban@live.com>
2023-03-25 12:47:09 -0500

    Walk to next tube animation

--- src/app/demo/components/game/game.component.ts
+++ src/app/demo/components/game/game.component.ts
@@ -1,16 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
     templateUrl: './game.component.html',
     styleUrls: ['./game.component.scss']
 })
-export class GameComponent {
+export class GameComponent implements OnInit {
     isPaused = true;
+    isFromQuiz = false;
+
+    constructor(
+        private router: Router,
+    ) { }
 
     nextQuiz() {
         this.isPaused = false;
         setTimeout(() => {
             this.isPaused = true;
-        }, 10000);
+        }, 4 * 1000);
     }
+
+    ngOnInit() {
+        this.isFromQuiz = this.router.url.includes('fromquiz');
+    }
+
 }
