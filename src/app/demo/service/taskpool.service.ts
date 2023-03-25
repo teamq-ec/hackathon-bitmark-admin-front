db2d0a0b75b90368edbd05ae07154fb5057ded2b
juanjoteamq <jfc@teamq.biz>
2023-03-25 16:28:34 -0500

    add skeleton style n feedback service

--- src/app/demo/service/taskpool.service.ts
+++ src/app/demo/service/taskpool.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -16,5 +16,15 @@ export class TaskpoolService {
 
   getExercise(word: any){
     return this.http.get<any>("api/exercises?translationPair=de->en&word="+word);
+  }
+
+  getFeedBack(body: any){
+    let api_key = "pqsq62oYyilKbG4936Nja-uUqjg";
+    const headers = new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${api_key}`
+      });
+    const requestOptions = { headers: headers };
+    return this.http.post<any>("apiFeedBack/feedback/compute", body, requestOptions);
   }
 }
