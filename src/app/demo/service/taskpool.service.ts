4184ed79fb89326495d9dea035b68eb8e4559752
gabyteamq <ggc@teamq.biz>
2023-03-25 17:58:18 -0500

    test env

--- src/app/demo/service/taskpool.service.ts
+++ src/app/demo/service/taskpool.service.ts
@@ -5,14 +5,14 @@ import { Injectable, isDevMode } from '@angular/core';
   providedIn: 'root'
 })
 export class TaskpoolService {
-    baseUrl: string = isDevMode() ? 'api/' : 'https://taskpool.taskbase.com/';
+    baseUrl: string = isDevMode() ? '' : 'https://taskpool.taskbase.com/';
 
   constructor(
     private http: HttpClient
   ) { }
 
   getWords() {
-    return this.http.get<any>(this.baseUrl + "words?translationPair=de->en", {
+    return this.http.get<any>(this.baseUrl + "api/words?translationPair=de->en", {
         headers: {
             'Access-Control-Allow-Origin': '*',
             'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept'
@@ -21,7 +21,7 @@ export class TaskpoolService {
   }
 
   getExercise(word: any){
-    return this.http.get<any>(this.baseUrl + "exercises?translationPair=de->en&word="+word);
+    return this.http.get<any>(this.baseUrl + "api/exercises?translationPair=de->en&word="+word);
   }
 
   getFeedBack(body: any){
